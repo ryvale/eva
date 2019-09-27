413945638615d56d487f1df5c852a3b9d4fcd34a
ryvale <valery.nzi@2m2slimited.com>
2019-09-27 13:13:40 +0000

    Null type refactored

--- eva/src/main/java/com/exa/expression/eval/XPEvaluator.java
+++ eva/src/main/java/com/exa/expression/eval/XPEvaluator.java
@@ -2,7 +2,6 @@ package com.exa.expression.eval;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
